70db95505d069d673668e82ad875299bd02a15da
Pablo Fernandez Aduviri <pablo.juegos12345@gmail.com>
2022-08-10 20:24:24 -0400

    listo para produccion

--- src/app/pages/item/item.component.ts
+++ src/app/pages/item/item.component.ts
@@ -1,4 +1,7 @@
+import { ProductoDescripcion } from './../../interfaces/producto-descripcion.interface';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductosService } from 'src/app/services/productos.service';
 
 @Component({
   selector: 'app-item',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemComponent implements OnInit {
 
-  constructor() { }
+
+  //OTRA MANERA DE REALIZARLO SERIA PONIENDO OPCIOMNALES LOS ATYRIBUTOS DE LA INTERFAZ  con ?
+  producto!: ProductoDescripcion;
+
+  id!: string;
+
+  constructor( private route: ActivatedRoute,
+              public productoService: ProductosService ) { }
 
   ngOnInit(): void {
+
+
+    this.route.params.subscribe(
+      parametros => {
+        // console.log(parametros['id']);
+        this.productoService.getProducto(parametros['id']).subscribe(
+          //Carajo de nuevo no me deja poner la interface en la respuesta pero si funciono y luego quize usar una propiedad y ya no
+          (producto: any) => {
+            this.id = parametros['id'];
+            this.producto = producto;
+            // console.log(producto);
+          }
+        )
+      }
+    )
   }
 
 }
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -9,22 +9,67 @@ export class ProductosService {
 
   cargando = true;
   productos: Producto[] = [];
+  productosFiltrado: Producto[] = [];
 
   constructor(private http: HttpClient) {
     this.cargarProductos();
   }
 
   private cargarProductos(){
-
-    //DEBO RESOLVER EL ERROR DE QUE NO ME DEJA RECONOCER LA INTERFACE
-    this.http.get('https://angular-html-pablo-default-rtdb.firebaseio.com/productos_idx.json').subscribe(
+    return new Promise<void>( (resolve, reject) => {
+      //DEBO RESOLVER EL ERROR DE QUE NO ME DEJA RECONOCER LA INTERFACE
+      this.http.get('https://angular-html-pablo-default-rtdb.firebaseio.com/productos_idx.json').subscribe(
       (resp: any)=> {
-        console.log(resp);
         this.productos = resp;
-        
         setTimeout(() => {
           this.cargando = false;
         }, 1000);
+        resolve();
       });
+    });
+  }
+
+  getProducto(id: string){
+
+//PODRIAS ENCONTRAR UNA FORMA AGRADABLE DE QUE EL USUARIO RECIBA ESA PANTALLA DE CARGA 
+    //Este es un observable
+    return this.http.get(`https://angular-html-pablo-default-rtdb.firebaseio.com/productos/${ id }.json`)
+  }
+
+
+  buscarProducto(termino: string){
+
+    if( this.productos.length === 0 ){
+      //cargar productos
+      this.cargarProductos().then(()=>{
+        //ejecutar despues de tener los productos
+        //Aplicar Filtro
+        this.filtrarProductos(termino);
+      });
+    } else {
+      //aplicar el filtro
+      this.filtrarProductos(termino);
+
+    }
+    
+    
+  }
+
+  private filtrarProductos( termino: string ) {
+    // console.log(this.productos);
+    this.productosFiltrado = [];
+
+    termino = termino.toLocaleLowerCase();
+
+    this.productos.forEach( prod => {
+      const tituloLower = prod.titulo.toLocaleLowerCase();
+      if (prod.categoria.indexOf( termino ) >= 0 || tituloLower.indexOf(termino) >= 0) {
+        this.productosFiltrado.push(prod)
+      }
+    });
+    // this.productosFiltrado = this.productos.filter( producto => {
+    //   return true;
+    // });
+    // console.log(this.productosFiltrado);
   }
 }
